0e8fc936271b1b8ad08f3547aa23b4942005b65a
vaalarmo <valeria.araya.m@gmail.com>
2018-09-11 13:08:05 -0300

    css

--- src/app/component/movil/facturacion/facturacion.component.ts
+++ src/app/component/movil/facturacion/facturacion.component.ts
@@ -16,8 +16,8 @@ import { DialogDetalleFacturaComponent } from '../dialog-detalle-factura/dialog-
   styleUrls: ['./facturacion.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({display: 'none'})),
+      state('expanded', style({height: '*', display:'*'})),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
